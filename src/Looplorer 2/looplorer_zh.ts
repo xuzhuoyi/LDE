80e558aca949dae055a72a536a08fe65c0c13107
xuzhuoyi <xzy476386434@vip.qq.com>
2014-11-22 21:01:21 +0800

    Rewrite Looplorer in Qt5Webkit

---
+++ src/Looplorer 2/looplorer_zh.ts
@@ -0,0 +1,78 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.cpp" line="74"/>
+        <source>Navigation</source>
+        <translation type="unfinished">导航</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="82"/>
+        <source>&amp;View</source>
+        <translation type="unfinished">视图(&amp;V)</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="83"/>
+        <source>Page Source</source>
+        <translation type="unfinished">页面源代码</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="88"/>
+        <source>&amp;Effect</source>
+        <translation type="unfinished">效果(&amp;E)</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="89"/>
+        <source>Highlight all links</source>
+        <translation type="unfinished">高亮所有链接</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="94"/>
+        <source>Turn images upside down</source>
+        <translation type="unfinished">倒转图像</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="98"/>
+        <source>&amp;Tools</source>
+        <translation type="unfinished">工具(&amp;T)</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="99"/>
+        <source>Remove GIF images</source>
+        <translation type="unfinished">屏蔽 GIF 图像</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="100"/>
+        <source>Remove all inline frames</source>
+        <translation type="unfinished">屏蔽所有内联框架</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="101"/>
+        <source>Remove all object elements</source>
+        <translation type="unfinished">屏蔽所有对象元素</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="102"/>
+        <source>Remove all embedded elements</source>
+        <translation type="unfinished">屏蔽所有嵌入元素</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="104"/>
+        <source>&amp;Help</source>
+        <translation type="unfinished">帮助(&amp;H)</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="105"/>
+        <location filename="mainwindow.cpp" line="229"/>
+        <source>About Looplorer</source>
+        <translation type="unfinished">关于 Looplorer</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="229"/>
+        <source>Looplorer 1.9.1</source>
+        <translation type="unfinished">Looplorer 1.9.1</translation>
+    </message>
+</context>
+</TS>
