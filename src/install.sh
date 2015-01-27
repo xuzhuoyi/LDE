09a86b714dcbaec6666bf0eedd88dbf75b2c1bf1
xuzhuoyi <xzy476386434@vip.qq.com>
2015-01-27 22:30:02 +0800

    Add install.sh Setup Tool

---
+++ src/install.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+install ../usr/bin/* /usr/bin/
+install ../share/*.desktop /usr/share/applications/
+install ../shell/startlde /usr/bin/
+install ../xsessions/LDE.desktop /usr/share/xsessions/
