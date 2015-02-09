7e957f4fbc904fc42e0d6b50e60693c69cc0de67
xuzhuoyi <xzy476386434@vip.qq.com>
2015-02-09 22:42:38 +0800

    Modify debian/*

--- debian/lde.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the lde package
-#
-0 4	* * *	root	[ -x /usr/bin/lde_maintenance ] && /usr/bin/lde_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(lde):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="lde" command="/usr/bin/lde"
