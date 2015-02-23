97c56a1aca9ccd3a3ce74d82b963f328596da213
xuzhuoyi <xzy476386434@vip.qq.com>
2015-02-23 23:01:41 +0800

    Add Wallpaper in Makefile

--- src/install.sh
+++ src/install.sh
@@ -3,3 +3,4 @@ install ../usr/bin/* /usr/bin/
 install ../share/*.desktop /usr/share/applications/
 install ../shell/startlde /usr/bin/
 install ../xsessions/LDE.desktop /usr/share/xsessions/
+install ../wallpapers/lde-default.png /usr/share/wallpapers/
