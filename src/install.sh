ef467b0aa3a389e2f4b2e2b9b7687b04f75357af
xuzhuoyi <xzy476386434@vip.qq.com>
2015-02-26 18:04:09 +0800

    Update install.sh

--- src/install.sh
+++ src/install.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
-install ../usr/bin/* /usr/bin/
-install ../share/*.desktop /usr/share/applications/
-install ../shell/startlde /usr/bin/
-install ../xsessions/LDE.desktop /usr/share/xsessions/
-install ../wallpapers/lde-default.png /usr/share/wallpapers/
+install -d /usr/
+install -d /usr/bin/
+install -d /usr/share/
+install -d /usr/share/applications/
+install -d /usr/share/xsessions/
+install -d /usr/share/wallpapers/
+install -d /usr/share/apps/kdm/sessions/
+install usr/bin/* /usr/bin/
+install share/*.desktop /usr/share/applications/
+install shell/startlde /usr/bin/
+install xsessions/LDE.desktop /usr/share/xsessions/
+install wallpapers/lde-default.png /usr/share/wallpapers/
+install xsessions/LDE.desktop /usr/share/apps/kdm/sessions/
