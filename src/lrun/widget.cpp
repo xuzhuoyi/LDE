f009effaff7c3e7b4e4ea1e68591a7c7529223f4
xuzhuoyi <xzy476386434@vip.qq.com>
2015-08-23 14:20:04 +0800

    Add QFileDialog in LRun

--- src/lrun/widget.cpp
+++ src/lrun/widget.cpp
@@ -1,6 +1,8 @@
 #include "widget.h"
 #include "ui_widget.h"
 #include <QProcess>
+#include <QFileDialog>
+#include <QMessageBox>
 
 Widget::Widget(QWidget *parent) :
     QWidget(parent),
@@ -24,4 +26,13 @@ void Widget::on_pushButton_clicked()
     QProcess *mainProcess = new QProcess;
     mainProcess->start(ui->lineEdit->text(),NULL);
     qApp->quit();
+}
+
+void Widget::on_pushButton_3_clicked()
+{
+    path = QFileDialog::getOpenFileName(this, tr("Run..."), ".");
+    if(path.length() == 0) {
+        QMessageBox::information(NULL, tr("Run"), tr("You didn't select any files."));
+    }
+    ui->lineEdit->setText(path);
 }
--- src/lrun/widget.h
+++ src/lrun/widget.h
@@ -20,8 +20,11 @@ private slots:
 
     void on_pushButton_clicked();
 
+    void on_pushButton_3_clicked();
+
 private:
     Ui::Widget *ui;
+    QString path;
 };
 
 #endif // WIDGET_H
